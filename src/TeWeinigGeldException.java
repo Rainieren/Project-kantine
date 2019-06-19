56da9fc569bff3d47061008ba4e0b0778c9b0817
Anna <annavanrosmalen3@gmail.com>
2019-06-19 19:59:30 +0200

    TeWeinigGeldException

---
+++ src/TeWeinigGeldException.java
@@ -0,0 +1,8 @@
+public class TeWeinigGeldException extends Exception {
+
+    public TeWeinigGeldException(){}
+
+    public TeWeinigGeldException(Exception e){}
+
+    public TeWeinigGeldException(String Message){}
+}
